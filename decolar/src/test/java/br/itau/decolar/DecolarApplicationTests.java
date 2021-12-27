4692de8cb68ad3709dae0e5f4096f4b31ca2bbfb
filipeluiz <ffilipeluiz@gmail.com>
2021-12-27 19:11:03 -0300

    Adicionando o projeto padrão para API Rest com SpringBoot

---
+++ decolar/src/test/java/br/itau/decolar/DecolarApplicationTests.java
@@ -0,0 +1,13 @@
+package br.itau.decolar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DecolarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
